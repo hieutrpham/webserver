50b89c62095170832270d3c07dd12dfdc2976160
hieutrpham <hieutrungpham91@gmail.com>
2026-04-09 15:28:32 +0300

    update

---
+++ Makefile
@@ -0,0 +1,44 @@
+CC = c++ -std=c++20
+NAME = webserv
+CFLAGS = -Wall -Werror -Wextra
+DFLAGS = -MMD -MP
+
+# sources
+SRC_PATH = src/
+SRC = main.cpp
+
+# objects
+OBJ = $(SRC:.cpp=.o)
+OBJ_PATH = obj/
+OBJS = $(addprefix $(OBJ_PATH), $(OBJ))
+
+# dependencies
+DEP = $(addprefix $(OBJ_PATH), $(SRC_PATH:.cpp=.d))
+
+all: $(OBJ_PATH) $(NAME)
+
+$(OBJ_PATH)%.o: $(SRC_PATH)%.cpp
+	$(CC) $(CFLAGS) $(DFLAGS) -o $@ -c $<
+
+$(OBJ_PATH):
+	@mkdir -p $(OBJ_PATH)
+
+.SECONDARY: $(OBJS)
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $^
+
+-include $(DEP)
+
+clean:
+	rm -rf $(OBJ_PATH)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+debug:
+	$(MAKE) CFLAGS="$(CFLAGS) -ggdb -DDEBUG" re
+
+.PHONY: all clean fclean re debug
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
+#include <sys/poll.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <asm-generic/socket.h>
 #include <unistd.h>
 #include <cstdio>
 #include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netdb.h>
 #include <string.h>
 
 #define PORT 8888
+#define ARRAY_LEN(arrays) (sizeof(arrays)/sizeof(arrays[0]))
 
 int main() {
-	const char *hello = "HTTP/1.1 413 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
 
 	// create a socket
 	int server_fd;
@@ -25,8 +27,9 @@ int main() {
 	struct sockaddr_in address;
 	socklen_t addrlen = sizeof(address);
 
+	in_addr_t addr = inet_addr("127.0.0.2");
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_addr.s_addr = addr;
 	address.sin_port = htons(PORT);
 	memset(address.sin_zero, 0, sizeof(address.sin_zero));
 
@@ -43,10 +46,11 @@ int main() {
 	}
 	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, addrlen);
 
+	int count = 0;
+	std::string hello = "HTTP/1.1 413 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
 	int new_socket;
 	sockaddr_in client;
 	while (true) {
-		// new_socket = 0;
 		if ((new_socket = accept(server_fd, (struct sockaddr*)&client, &addrlen)) < 0) {
 			perror("ERR: this is unacceptable");
 			return 0;
@@ -56,12 +60,21 @@ int main() {
 			perror("ERR: fork");
 		if (child_fd == 0) {
 			close(server_fd);
-			if (send(new_socket, hello, strlen(hello), 0) < 0)
+			if (send(new_socket, hello.c_str(), hello.length(), 0) < 0)
 				perror("ERR:send");
 			close(new_socket);
 			exit(0);
 		}
-		close(new_socket);
+		if (child_fd > 0) {
+			std::cout << "client " << count << " connected\n";
+			count++;
+			char buffer[30000] = {0};
+			if (read(new_socket, buffer, sizeof(buffer)) < 0)
+				perror("ERR");
+			std::cout << buffer << std::endl;
+			close(new_socket);
+		}
+		wait(NULL);
 	}
 
 	close(server_fd);
