d8f302421d53b953be3d1eff0fe7f6571d4bc788
Hieu Pham <hieutrungpham91@gmail.com>
2026-07-14 12:21:09 +0300

    fix: parsing of file upload and exception handling to return error response

--- src/POSTMethod.cpp
+++ src/POSTMethod.cpp
@@ -23,9 +23,17 @@ Response POSTMethod::handlePost(Request& request, ServerConfig& config)
 
 Response POSTMethod::handleFileUpload(std::string &content_type, Request& request, ServerConfig& config)
 {
-	auto name = save_file_upload(content_type, request, config);
-
 	Response response;
+	std::string name;
+
+	try {
+		name = save_file_upload(content_type, request, config);
+	} catch (std::exception &e) {
+		ERR(e.what());
+		response = ResponseBuilder::buildErrorResponse(500, "Internal Server Error", config);
+		return response;
+	}
+
 	response.setStatus(202, "Accepted");
 	response.setHeader("Location", name);
 	response.setHeader("Content-Type", "text/html");
@@ -37,20 +45,24 @@ std::string POSTMethod::get_file_name(std::string &body)
 {
 	std::istringstream body_stream(body);
 	std::string line;
-	std::string name;
+	std::string file_name;
 
 	// get name of the file
 	while (std::getline(body_stream, line))
 	{
-		if (line.find("filename") != std::string::npos)
+		auto file_name_index = line.find("filename");
+		if (file_name_index != std::string::npos)
 		{
-			name = line.substr(line.find("filename=\""));
-			name = name.substr(10, name.find("\"") - 1);
+			auto name = line.substr(file_name_index);
+			auto first_quote = name.find("\"") + 1;
+			name = name.substr(first_quote);
+			auto second_quote = name.find("\"");
+			file_name = name.substr(0, second_quote);
 			break;
 		}
 	}
 
-	return name;
+	return file_name;
 }
 
 bool POSTMethod::is_file_upload(std::string &content_type, Request &request, ServerConfig & config)
@@ -71,7 +83,7 @@ std::string POSTMethod::save_file_upload(std::string &content_type, Request &req
 	std::fstream outfile(name, outfile.out);
 	if (!outfile.is_open())
 	{
-		throw std::runtime_error("could not open file: " + name);
+		throw std::runtime_error("Could not open file: " + name);
 	}
 
 	// extracting out main body between boundary
