bafe948dad0bd0ebe9f5421ae8b88ea27434353b
Waltteri Heino <waltteriheino@gmail.com>
2026-07-22 15:22:48 +0300

    Fixed delete method not working

--- src/GETMethod.hpp
+++ src/GETMethod.hpp
@@ -7,15 +7,15 @@
 
 class GetMethod {
 	private:
-		static bool matchLocation(const std::string& requestPath, const ServerConfig& config, Location& matchedLocation);
-		static bool pathExists(const std::string& path);
-		static bool isDirectory(const std::string& path);
-		static bool isRegularFile(const std::string& path);
-		static std::string getMimeType(const std::string& path);
-		static bool endsWith(const std::string& str, const std::string& suffix);
-		static Response generateAutoIndex(const std::string& dirPath, const std::string& requestPath, ServerConfig& config);
-
+	static bool pathExists(const std::string& path);
+	static bool isDirectory(const std::string& path);
+	static bool isRegularFile(const std::string& path);
+	static std::string getMimeType(const std::string& path);
+	static bool endsWith(const std::string& str, const std::string& suffix);
+	static Response generateAutoIndex(const std::string& dirPath, const std::string& requestPath, ServerConfig& config);
+	
 	public:
+		static bool matchLocation(const std::string& requestPath, const ServerConfig& config, Location& matchedLocation);
 		static Response handleGet(Request& request, ServerConfig& config);
 };
 
