eca729403d6f6e7b59f3aa1928ce393e59360ad9
Waltteri Heino <waltteriheino@gmail.com>
2026-07-22 14:50:45 +0300

    Added robust final test site

---
+++ websites/website_1/cgi-bin/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+
+sys.stdout.write("Content-Type: text/plain\r\n\r\n")
+# sys.stdout.flush()
+
+# time.sleep(5)
+print("why am i a header :  is this value")
